e633d10484dfecc2d4edfcb33fdf3f64ed78346f
sawroar <sawroarhossain5@gmail.com>
2022-11-30 16:13:25 +0600

    environemnt veriable added

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
-
+require('dotenv').config()
 
 // middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wzcp5kk.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log)
+
 
 app.get('/', async (req, res) => {
     res.send('Car Way server is running');
